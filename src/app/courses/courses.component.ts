4d6369f9fbfb9d08089fdfb92c10e9c82771080c
Rodrigues <fr.costa95@gmail.com>
2020-03-04 22:36:16 -0300

    learn(routes): learn about query parameters

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 import { CoursesService } from './courses.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,13 +12,27 @@ import { CoursesService } from './courses.service'
 export class CoursesComponent implements OnInit {
 
   courses: any[];
+  page: number;
+  subscription: Subscription;
 
-  constructor(private _coursesService: CoursesService) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private _coursesService: CoursesService
+    ) { }
 
   ngOnInit() {
 
     this.courses = this._coursesService.getCourses();
 
+    this.subscription = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.page = queryParams['page'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
